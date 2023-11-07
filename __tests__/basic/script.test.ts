a8bc2cfa1a67109618abafc88e3177c29f915cab
Ken Takahashi <takaken@redimpulz.com>
2023-11-07 14:08:57 +0900

    テストケースを追加

--- __tests__/basic/script.test.ts
+++ __tests__/basic/script.test.ts
@@ -4,7 +4,9 @@ import { func1, func2 } from "../../basic/script";
 describe("ループ・配列の問題", () => {
   test("func1", () => expect(func1(5)).toEqual([1, 2, 3, 4, 5]));
   test("func1", () => expect(func1(8)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]));
+  test("func1", () => expect(func1(0)).toEqual([]));
   test("func1", () => expect(func1(-1)).toEqual([]));
+  test("func1", () => expect(func1("hogehoge")).toEqual([]));
 });
 
 describe("条件分岐・データ型の問題", () => {
@@ -14,5 +16,7 @@ describe("条件分岐・データ型の問題", () => {
   test("func2", () => expect(func2(15)).toEqual("FizzBuzz"));
   test("func2", () => expect(func2(22)).toEqual("22"));
   test("func2", () => expect(func2(30)).toEqual("FizzBuzz"));
+  test("func2", () => expect(func2(0)).toEqual("error"));
+  test("func2", () => expect(func2(-1)).toEqual("error"));
   test("func2", () => expect(func2("hogehoge")).toEqual("error"));
 });
