31a1f14212a896c1c5696e4d750f6bc986cbf32f
Ken Takahashi <takaken@redimpulz.com>
2023-11-21 17:09:04 +0900

    回答コードを追加

--- basic/script.js
+++ basic/script.js
@@ -1,9 +1,20 @@
 // ループ・配列・データ型の問題
 export const func1 = (n) => {
-  // このコードを完成させてください
+  if (!Number.isInteger(n)) return [];
+  if (n <= 0) return [];
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(i + 1);
+  }
+  return arr;
 };
 
 // 条件分岐・データ型の問題
 export const func2 = (n) => {
-  // このコードを完成させてください
+  if (!Number.isInteger(n)) return "error";
+  if (n <= 0) return "error";
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
+  return `${n}`;
 };
--- promise/script.js
+++ promise/script.js
@@ -1,14 +1,31 @@
 // 非同期処理
 export const func1 = async () => {
-  // このコードを完成させてください
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const json = await res.json();
+  console.log(json);
+  return json;
 };
 
 // 直列で非同期処理
 export const func2 = async () => {
-  // このコードを完成させてください
+  const ids = [1, 2, 3, 4, 5];
+  for (const id of ids) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    const json = await res.json();
+    console.log(json);
+  }
 };
 
 // 並列で非同期処理
 export const func3 = async () => {
-  // このコードを完成させてください
+  const ids = [1, 2, 3, 4, 5];
+  const fetches = ids.map((id) =>
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then((res) =>
+      res.json()
+    )
+  );
+  const jsons = await Promise.all(fetches);
+  return jsons;
 };
+
+func1();
